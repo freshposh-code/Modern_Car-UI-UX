e8da77bbd32d602f5187d87ce45f84da6c2bb264
freshposh-code <atomisefarouk919@gmail.com>
2024-01-04 02:06:13 +0100

    first commit

--- public/types/index.ts
+++ public/types/index.ts
@@ -59,5 +59,5 @@ export interface CustomFilterProps {
 export interface ShowMoreProps {
     pageNumber: number;
     isNext: boolean;
-    setLimit: Dispatch<SetStateAction<string>>
+    setLimit: Dispatch<SetStateAction<number>>
 }
