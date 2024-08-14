427aa40b21fff83ff9a1ab887a0896665934fa59
freshposh-code <atomisefarouk919@gmail.com>
2024-08-14 22:25:02 +0100

    pass env for API_KEY

--- Utils/index.ts
+++ Utils/index.ts
@@ -1,65 +1,66 @@
 import { CarProps, FilterProps } from "@/public/types";
 
 export async function fetchcars(filters: FilterProps) {
-    const  {manufacturer, year, model, limit, fuel} = filters;
-    const headers = { 
-        'X-RapidAPI-Key': 'ee3774757cmsh578da00a94fb746p1b3bb8jsnc178adc02f26',
-		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-    }
+  const { manufacturer, year, model, limit, fuel } = filters;
+  const headers = {
+    'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
+    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+  }
 
-    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${
-        manufacturer
+  const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer
     }&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
-        headers: headers,
-    });
+    headers: headers,
+  });
 
-    // Parse the response as JSON
+  // Parse the response as JSON
 
-    const result = await response.json()
+  const result = await response.json()
 
-    return result
+  return result
 }
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
-    const basePricePerDay = 50; // Base rental price per day in dollars
-    const mileageFactor = 0.1; // Additional rate per mile driven
-    const ageFactor = 0.05; // Additional rate per year of vehicle age
-  
-    // Calculate additional rate based on mileage and age
-    const mileageRate = city_mpg * mileageFactor;
-    const ageRate = (new Date().getFullYear() - year) * ageFactor;
-  
-    // Calculate total rental rate per day
-    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
-  
-    return rentalRatePerDay.toFixed(0);
-  };
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
 
-  
-  export const generateCarImageUrl = (car: CarProps, angle?: string) => {
-    const url = new URL("https://cdn.imagin.studio/getimage");
-    const { make, model, year } = car;
-  
-    url.searchParams.append('customer', 'hrjavascript-mastery' || '');
-    url.searchParams.append('make', make);
-    url.searchParams.append('modelFamily', model.split(" ")[0]);
-    url.searchParams.append('zoomType', 'fullscreen');
-    url.searchParams.append('modelYear', `${year}`);
-    // url.searchParams.append('zoomLevel', zoomLevel);
-    url.searchParams.append('angle', `${angle}`);
-  
-    return `${url}`;
-  } 
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
 
-  export const updateSearchParams = (type: string, value: string) => {
-    // Get the current URL search params
-    const searchParams = new URLSearchParams(window.location.search);
-  
-    // Set the specified search parameter to the given value
-    searchParams.set(type, value);
-  
-    // Set the specified search parameter to the given value
-    const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
-  
-    return newPathname;
-  };
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
+
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, model, year } = car;
+
+  url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGE_API_KEY || '');
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(" ")[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  url.searchParams.append('angle', `${angle || 'front'}`);
+
+  console.log(url.toString()); // Log to check the final URL
+
+  return url.toString(); // Ensure it's a string
+}
+
+
+export const updateSearchParams = (type: string, value: string) => {
+  // Get the current URL search params
+  const searchParams = new URLSearchParams(window.location.search);
+
+  // Set the specified search parameter to the given value
+  searchParams.set(type, value);
+
+  // Set the specified search parameter to the given value
+  const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+  return newPathname;
+};
